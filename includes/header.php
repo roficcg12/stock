c78df29fb7c1a5d5589690d74ca83c948e1bc0c0
rofi.17 <ritulmail1@gmail.com>
2018-03-05 20:38:37 +0530

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="rofi">
+    <meta name="description" content="stock management system, NIC, LCB College, BSc Final Year Project, Gauhati University">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="./assets/custom/css/style.css">
+    <link rel="stylesheet" href="./assets/custom/js/custom.js">
+    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+    <title>STOCK MANAGEMENT SYSTEM</title>
+</head>
+<body>
+    <div class="container">
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require_once './includes/header.php';?>
+    <h1 class="custom">this is SMS home page</h1>
+<?php require_once './includes/footer.php';?>
+
+
+
+
+
